4a1fbc5cef129b9d72cb985b4e89a90297495a85
Pathé DIAGNE <pathe.diagne@gmail.com>
2020-11-16 22:15:53 +0100

    Cliquer sur le titre pour aller sur la page de l'épisode

--- templates/frontoffice/posts.html.php
+++ templates/frontoffice/posts.html.php
@@ -3,7 +3,7 @@
     <p class="episode">Les épisodes</p>
     
     <?php foreach($data['allposts'] as $post): ?>    
-        <h3 class="post-title"><?=$post['titlePost']?></h3>
+        <h3 class="post-title"><a href="index.php?action=post&idPost=<?=$post['idPost']?>"><?=$post['titlePost']?></a></h3>
         <p class="publication-date">Publié le <?=$post['fr_creationDate']?></p>
         <p class="post-text-sample"><?=$post['textPost']?></p>
         <button class="post-link"><a  href="index.php?action=post&idPost=<?=$post['idPost']?>">Lire l'épisode</a></button>
