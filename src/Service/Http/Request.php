f8a81287367d7856ca3df456d2b42aaa8a11336e
Pathé DIAGNE <pathe.diagne@gmail.com>
2020-12-22 21:39:31 +0100

    Add getters in the class Request

--- src/Service/Http/Request.php
+++ src/Service/Http/Request.php
@@ -19,5 +19,30 @@ class Request
     {
         return count($this->post) === 0 ? null : $this->post;
     }
+
+    public function getAction(): string
+    {
+        return (string)$this->get['action'];
+    }
+
+    public function getPage(): int
+    {
+        return (int)$this->get['page'];
+    }
+
+    public function getIdPost(): int
+    {
+        return (int)$this->get['idPost'];
+    }
+
+    public function getIdComment(): int
+    {
+        return (int)$this->get['idComment'];
+    }
+
+    public function getIdAuthor(): int
+    {
+        return(int)$this->get['idAuthor'];
+    }
 }
 // class permettant la gestion des variables supers globales de php sauf $_SESSION
